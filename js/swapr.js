6ee2d48359ff8b5fec2153c22c45181794938610
Ehryk <rhaistlin@gmail.com>
2013-07-14 11:38:04 -0500

    Added check for rewritten request

--- js/swapr.js
+++ js/swapr.js
@@ -3,8 +3,32 @@
 //     (c) 2013 Eric Menze
 //     Swapr may be freely distributed under the MIT license.
 
+//Example Call:
+//Swapr(container, elements, progress, extension)
+//Swapr("#swap", ".nav .dropdown-menu li a", "#loading", "html")
+
+function checkRewrite() {
+    var href = window.location.href;
+    if (href.toLowerCase().indexOf('sections') != -1) {
+        //This is a section
+        var url = window.location.pathname;
+        if (url.toLowerCase().indexOf('.html') == -1)
+            url += '.html';
+
+        console.log('Rewritten Request, loading: ' + url);
+
+        $("#swap").load(url, function(response, status, xhr) {
+            $("#loading").hide();
+            if (status == "error") {
+                var msg = "Sorry but there was an error: ";
+                $("#error").html(msg + xhr.status + " " + xhr.statusText);
+            }
+        });
+    }
+};
+
 $(function () {
-    $('.dropdown a').click(function (e) {
+    $('.nav .dropdown-menu li a').click(function (e) {
         $("#loading").show();
         href = $(this).attr("href");
 
@@ -26,15 +50,23 @@ $(function () {
 
 function loadContent(url) {
     // USES JQUERY TO LOAD THE CONTENT
-    $.load("content.php", { cid: url, format: 'json' }, function (json) {
-        // THIS LOOP PUTS ALL THE CONTENT INTO THE RIGHT PLACES
-        $.each(json, function (key, value) {
-            $(key).html(value);
-        });
+    if (url == "#" || url == "/"){
         $("#loading").hide();
+        return;
+    }
+    var urlPage = url;
+    if (urlPage.toLowerCase().indexOf('.html') == -1)
+        urlPage += '.html';
+    console.log('Swapping: ' + urlPage);
+    $("#swap").load(urlPage, function(response, status, xhr) {
+        $("#loading").hide();
+        if (status == "error") {
+            var msg = "Sorry but there was an error: ";
+            $("#error").html(msg + xhr.status + " " + xhr.statusText);
+        }
     });
 
-    // THESE TWO LINES JUST MAKE SURE THAT THE NAV BAR REFLECTS THE CURRENT URL
-    $('li').removeClass('current');
-    $('a[href="' + url + '"]').parent().addClass('current');
+    // Update Nav Bar   
+    $('li').removeClass('active');
+    $('a[href="' + url + '"]').parent().addClass('active');
 }
