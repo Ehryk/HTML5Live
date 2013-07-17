0324020d97e5ddf26fe1fbef92d3a6738b58d5ae
Ehryk <rhaistlin@gmail.com>
2013-07-17 11:45:26 -0500

    Updated rewrite and swapr to work on any friendly urls (no .)
    Added (commented) version of a Swapr library

--- js/swapr.js
+++ js/swapr.js
@@ -8,16 +8,14 @@
 //Swapr("#swap", ".nav .dropdown-menu li a", "#loading", "html")
 
 function checkRewrite() {
-    var href = window.location.href;
-    if (href.toLowerCase().indexOf('sections') != -1) {
-        //This is a section
-        var url = window.location.pathname;
-        if (url.toLowerCase().indexOf('.html') == -1)
-            url += '.html';
+    var path = window.location.pathname;
+    if (path.indexOf('.') == -1) {
+        //This is a friendly URL
+        var page = path + '.html';
 
-        console.log('Rewritten Request, loading: ' + url);
+        console.log('Rewritten Request, loading: ' + page);
 
-        $("#swap").load(url, function(response, status, xhr) {
+        $("#swap").load(page, function(response, status, xhr) {
             $("#loading").hide();
             if (status == "error") {
                 var msg = "Sorry but there was an error: ";
@@ -27,7 +25,60 @@ function checkRewrite() {
     }
 };
 
-$(function () {
+// window.Swapr = (function (container, elements, progress, extension, debug) {
+
+//     var version = '1.1';
+
+//     $(elements).click(function (e) {
+//         $(progress).show();
+//         href = $(this).attr("href");
+
+//         Swapr.loadContent(href);
+
+//         //Add to Browser History
+//         history.pushState('', 'New URL: ' + href, href);
+//         e.preventDefault();
+//     });
+
+//     // Set Browser History for Back/Forward
+//     window.onpopstate = function (event) {
+//         $(progress).show();
+//         if (debug) console.log("pathname: " + location.pathname);
+//         loadContent(location.pathname);
+//     };
+
+//     Swapr.prototype = {
+
+//         loadContent: function (url, debug) {
+//                     if (this.e.style.display !== 'none') {
+//                         this.e.style.display = 'none';
+//                     } else {
+//                         this.e.style.display = '';
+//                     }
+//                     return this;
+//                 },
+
+//         toggle: function () {
+//                     if (this.e.style.display !== 'none') {
+//                         this.e.style.display = 'none';
+//                     } else {
+//                         this.e.style.display = '';
+//                     }
+//                     return this;
+//                 },
+
+//         size:   function (height, width) {
+//                     this.e.style.height = height + 'px';
+//                     this.e.style.width = width + 'px';
+//                     return this;
+//                 }
+//     };
+
+//     return Swapr;
+// })(window, true)
+
+
+$(function(){
     $('.nav .dropdown-menu li a').click(function (e) {
         $("#loading").show();
         href = $(this).attr("href");
